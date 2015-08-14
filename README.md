5179cf724bd55da31128685a4984162d1eabc7b4
marsweb <marswebb@qq.com>
2015-08-14 14:36:04 +0800

    add tab switch file

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # AvalonExp Avalon实例
 
-# avalonLogin.html
+## avalonLogin.html
 使用Avalon框架测试简单的登录功能
+
+## avalonTabSwitch.html
+使用Avalon框架实现选项卡切换功能
---
+++ js/avalonTabSwitch.js
@@ -0,0 +1,7 @@
+var tabBox=avalon.define({
+    $id: "tebBox",
+    vFlag: 0,
+    showTab: function(index){
+        tabBox.vFlag=index;
+    }
+});
